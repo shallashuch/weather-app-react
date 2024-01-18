83f3817b723c3609620d4b3c4a454ec70cf1429a
shallashuch <massa.ilaria.96@gmail.com>
2024-01-18 17:22:14 +0100

    fix

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "./WeatherInfo.css";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
