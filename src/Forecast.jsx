38beeb755d62724e27db67e2973afd27cbad6b71
shallashuch <massa.ilaria.96@gmail.com>
2024-01-24 11:17:41 +0100

    netlify

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useLayoutEffect, useInsertionEffect} from "react"
+import React, {useState, useEffect} from "react"
 import axios from "axios"
 import ForecastWeatherDays from "./ForecastWeatherDays"
 
